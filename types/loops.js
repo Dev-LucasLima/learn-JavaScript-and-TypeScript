8e77e384c112d776bce2b57d5a95e699543e1b9d
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-12-11 19:46:59 -0300

    feat: create a loop example

---
+++ types/loops.js
@@ -0,0 +1,16 @@
+/*
+for loop -> for (inicializable; condition; increment/decrement)
+*/
+
+for (let variable = 0; variable < 10; variable++) {
+    console.log("current value of variable: ", variable);
+}
+
+/*
+while loop -> while (condition)
+*/
+let counter = 0;
+while (counter < 10) {
+    console.log("current value of counter: ", counter);
+    counter++;
+}
